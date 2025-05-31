10f1a01048e96b10a99bc48478458390caed5aae
Gaurav Shukla <heygauravshukla@gmail.com>
2025-05-31 09:58:33 +0530

    feat: add Wrapper component

---
+++ components/wrapper.tsx
@@ -0,0 +1,22 @@
+import { cn } from "@/lib/utils";
+import { createElement, ElementType } from "react";
+
+export function Wrapper<T extends ElementType = "div">({
+  as,
+  className,
+  children,
+}: {
+  as?: T;
+  className?: string;
+  children: React.ReactNode;
+}) {
+  const Component = as || "div";
+
+  return createElement(
+    Component,
+    {
+      className: cn("mx-auto max-w-4xl px-4 md:px-6", className),
+    },
+    children,
+  );
+}
