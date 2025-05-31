7871646e571b4cdfe1611c95667fc5a1a0f64bcd
Gaurav Shukla <heygauravshukla@gmail.com>
2025-05-31 11:09:02 +0530

    feat: integrate next-view-transitions for enhanced layout transitions

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import type { Metadata } from "next";
+import { ViewTransitions } from "next-view-transitions";
 import { Inter, Roboto_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -27,21 +28,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      suppressHydrationWarning
-      className={`${inter.variable} ${roboto_mono.variable} antialiased`}
-    >
-      <body>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+    <ViewTransitions>
+      <html
+        lang="en"
+        suppressHydrationWarning
+        className={`${inter.variable} ${roboto_mono.variable} antialiased`}
+      >
+        <body>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ViewTransitions>
   );
 }
