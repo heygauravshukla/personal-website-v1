e612f8a04a1e449c99af3d2babe6751f47d28c6f
Gaurav Shukla <heygauravshukla@gmail.com>
2025-06-27 16:23:29 +0530

    feat: replace ProjectsSection component with SectionIntro and ProjectsList on the Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,26 @@
 import { HeroSection } from "@/components/hero-section";
+import { Wrapper } from "@/components/wrapper";
+import { SectionIntro } from "@/components/section-intro";
+import { ProjectsList } from "@/components/projects-list";
 import { ArticlesSection } from "@/components/articles-section";
-import { ProjectsSection } from "@/components/projects-section";
 import { TimelineSection } from "@/components/timeline-section";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
-      <ProjectsSection limit={3} />
+      <hr />
+      <section className="py-20 md:py-32">
+        <Wrapper className="space-y-12">
+          <SectionIntro
+            heading="Projects"
+            subheading="I love building things."
+            paragraph="Here are some of the projects I've worked on."
+          />
+          <ProjectsList limit={3} />
+        </Wrapper>
+      </section>
+      <hr />
       <ArticlesSection limit={3} />
       <TimelineSection />
     </main>
